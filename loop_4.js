911cb6f662ab87c47ce588b4c86ff6f6c0b69ae8
Karthik Bommanal <karthikb1105@gmail.com>
2024-11-28 08:47:59 +0530

    Removed the files from Assignment1

--- loop_4.js
+++
@@ -1,9 +0,0 @@
-// Create a program that loops through an array of colors using the for of loop and prints each color.
-
-let arr_1=["Black","Red","Orange","Blue","Green","Yellow"]
-
-for(let x of arr_1)
-{
-    console.log("The Array of Colors are ==>" ,x);
-    
-}
--- loop_5.js
+++
@@ -1,9 +0,0 @@
-// Write a program that uses a for in loop to loop through an object and prints the key-value pairs.
-let obj_1=["Black","Red","Blue","Yellow","Green"]
-
-for(let x in obj_1)
-{
-    // console.log(x + " Is the Key Value\n" + obj_1[x]+ " Is the value associated with it\n")
-    console.log(`${x} is the key value ${obj_1[x]} is the value associated with it`);
-    
-}
